95c86d1cfeefe2513de5e3d9555c33c08ad720d1
amit-s-k <amitsk2014@gmail.com>
2019-01-28 14:35:12 +0530

    Added properties file

--- src/main/java/com/example/microservice/MicroserviceApplication.java
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -3,9 +3,11 @@ package com.example.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
+@PropertySource(value = {"classpath:bootstrap.yml","classpath:config-server.yml"})
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
