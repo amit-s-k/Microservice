13c82453fe8ce92e6bc9bed3a017173215e31f5e
naneeth <naneethc003@gmail.com>
2019-01-28 19:41:55 +0530

    added bootstrap.yml

--- src/main/java/com/example/microservice/MicroserviceApplication.java
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -3,7 +3,9 @@ package com.example.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MicroserviceApplication {
 
